9092078455df0432a962b45d939c5ec16529b229
Betta <betta@Bettas-MacBook-Pro.local>
2023-04-30 07:15:36 +0100

    included is admin middleware to fetch all user

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const { createUser, loginUser, getAllUsers, getUser, deleteUser, updateUser} = require('../controller/userController');
+const { verifyToken, isAdmin } = require('../middleware/jwtToken');
 
 router = express.Router();
 
 
 router.post('/register', createUser);
 router.post('/login', loginUser);
-router.get('/users', getAllUsers);
+router.get('/users', verifyToken, isAdmin, getAllUsers);
 router.get('/user/:id', getUser);
 router.delete('/delete/:id', deleteUser);
 router.put('/update/:id', updateUser);
