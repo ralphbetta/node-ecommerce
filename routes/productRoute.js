0b8b0bf0d26bf226e911051868e6b94ba4deefde
Betta <betta@Bettas-MBP.lan>
2023-05-02 07:55:23 +0100

    added middleware to product update route for loggedIn Users and admin

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { createProduct, getProduct, getAllProduct, editProduct } = require('../controller/productController');
+const { verifyToken, isAdmin } = require('../middleware/jwtToken');
 const router = express.Router();
 
 
 router.post('/create', createProduct);
 router.get('/:id', getProduct);
 router.get('/', getAllProduct);
-router.put('/:id', editProduct);
+router.put('/:id', verifyToken, isAdmin, editProduct);
 
 module.exports = router;
