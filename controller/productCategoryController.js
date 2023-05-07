531f6e20997db62050db1669ab08ce27412495df
Betta <betta@Bettas-MacBook-Pro.local>
2023-05-07 02:51:30 +0100

    completed update and fetch specific product

--- controller/productCategoryController.js
+++ controller/productCategoryController.js
@@ -1,12 +1,12 @@
 const ProductCategory = require('../model/productCatModel');
 const winston = require('winston'); //advange error logger
 
-const createProductCategory  = async (req, res) => {
+const createProductCategory = async (req, res) => {
 
-    ProductCategory.create(req.body).then((response)=>{
+    ProductCategory.create(req.body).then((response) => {
         res.status(200).json({ message: "Succesful", data: response });
-    }).catch((error)=>{
-       // winston.error(error.stack);
+    }).catch((error) => {
+        // winston.error(error.stack);
         logger.warn(error);
         res.status(500).json({ message: "Server Error", data: error });
     });
@@ -15,36 +15,47 @@ const createProductCategory  = async (req, res) => {
 
 const logger = winston.createLogger({
     transports: [
-      new winston.transports.Console()
+        new winston.transports.Console()
     ]
-  });
-  
+});
 
 
-const updateProductCategory  = async (req, res) => {
-    
+
+const updateProductCategory = async (req, res) => {
+
+    const id = req.params.id;
+    ProductCategory.findByIdAndUpdate(id, req.body, { new: true }).then((response) => {
+        res.status(200).json({ message: "Updated Succesful", data: response });
+    }).catch((error) => {
+        res.status(500).json({ message: "Server Error", data: error });
+    });
 
 }
 
 
-const getProductCategory  = async (req, res) => {
-    
-}
-
-const getAllProductCategory  = async (req, res) => {
-    ProductCategory.find().then((response)=>{
+const getProductCategory = async (req, res) => {
+    const id = req.params.id;
+    ProductCategory.findById(id).then((response) => {
         res.status(200).json({ message: "Succesful", data: response });
-    }).catch((error)=>{
+    }).catch((error) => {
         res.status(500).json({ message: "Server Error", data: error });
     });
 }
 
-const deleteProductCategory  = async (req, res) => {
-    
+const getAllProductCategory = async (req, res) => {
+    ProductCategory.find().then((response) => {
+        res.status(200).json({ message: "Succesful", data: response });
+    }).catch((error) => {
+        res.status(500).json({ message: "Server Error", data: error });
+    });
+}
+
+const deleteProductCategory = async (req, res) => {
+
 }
 
 module.exports = {
-    createProductCategory, 
+    createProductCategory,
     updateProductCategory,
     getProductCategory,
     getAllProductCategory,
