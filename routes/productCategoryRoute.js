3807ceb8a188286e4ecd773aa91e051ce9b335e2
Betta <betta@Bettas-MacBook-Pro.local>
2023-05-07 02:56:30 +0100

    complete product cat routes

--- routes/productCategoryRoute.js
+++ routes/productCategoryRoute.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const { verifyToken, isAdmin } = require('../middleware/jwtToken');
-const { createProductCategory } = require('../controller/productCategoryController');
+const { createProductCategory, getAllProductCategory, getProductCategory, updateProductCategory, deleteProductCategory } = require('../controller/productCategoryController');
 const router = express.Router();
 
 
 router.post('/', verifyToken, createProductCategory);
+router.get('/', verifyToken, getAllProductCategory);
+router.get('/:id', verifyToken, getProductCategory);
+router.put('/:id', verifyToken, updateProductCategory);
+router.delete('/:id', verifyToken, deleteProductCategory);
 
 
 module.exports = router;
